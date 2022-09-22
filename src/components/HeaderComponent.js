881ee3c459e39a81e1ad5da3c6f235bdf8cfb38f
Rishabh Vikram Singh <69898767+rishabh-glitch@users.noreply.github.com>
2022-09-22 12:25:28 +0530

    Create HeaderComponent.js

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const HeaderComponent = () => {
+  return (
+    <Navbar style={{ color:"yellow", background:"red"}} expand="lg">
+      <Container 
+      style= {{maxWidth:"1300px"}}
+      >
+        <Navbar.Brand href="#home">Parking System</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">BookSlots</Nav.Link>
+            <Nav.Link href="#link">BookSlots</Nav.Link>
+            <Nav.Link href="#link">Create Premise</Nav.Link>
+            <Nav.Link href="#link">Create Floor</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+  )
+}
+
+export default HeaderComponent
+
+
