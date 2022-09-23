e9f0c874abfccdf7e6263e05b0ffb584fac6663f
Rishabh Vikram Singh <69898767+rishabh-glitch@users.noreply.github.com>
2022-09-23 12:45:48 +0530

    Update HeaderComponent.js

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,23 +2,23 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const HeaderComponent = () => {
   return (
-    <Navbar style={{ color:"yellow", background:"red"}} expand="lg">
+    <Navbar style={{background:"radial-gradient(rgb(0 0 0), rgba(72, 67, 67, 0))"}} expand="lg">
       <Container 
       style= {{maxWidth:"1300px"}}
       >
-        <Navbar.Brand href="#home">Parking System</Navbar.Brand>
+        <Navbar.Brand href="#home" style = {{color:"aliceblue"}}>Parking System</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">BookSlots</Nav.Link>
-            <Nav.Link href="#link">BookSlots</Nav.Link>
-            <Nav.Link href="#link">Create Premise</Nav.Link>
-            <Nav.Link href="#link">Create Floor</Nav.Link>
+          <Nav className="me-auto" >
+            <Nav.Link href="#home" style = {{color:"aliceblue"}}>Home</Nav.Link>
+            <Link to="/bookparkingslot" className='nav-link active' style = {{color:"aliceblue"}}>BookSlots</Link>
+            <Nav.Link href="#link" style = {{color:"aliceblue"}}>Create Premise</Nav.Link>
+            <Nav.Link href="#link" style = {{color:"aliceblue"}}>Create Floor</Nav.Link>
             {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
