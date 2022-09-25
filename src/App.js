9b1d1cc07501e3bfb2291159694608d6a84d8627
Rishabh Vikram Singh <69898767+rishabh-glitch@users.noreply.github.com>
2022-09-25 10:33:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter ,Route, Switch} from 'react-router-dom';
 import HeaderComponent from './components/HeaderComponent';
 import BookParkingSlotComponent from './components/BookParkingSlotComponent';
-
+import BookedParkingSlot from './components/BookedParkingSlot';
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <Switch>
         <Route exact path = "/" component = {HeaderComponent}></Route> 
           <Route path = "/bookparkingslot" component = {BookParkingSlotComponent}></Route>
+          <Route path = "/bookedparkingslot" component = {BookedParkingSlot}></Route>
         </Switch>
       </div>
       </BrowserRouter>
