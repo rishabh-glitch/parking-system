0546cf99b1b6186da7c17e4bd6a4e6a41c1edfb8
Rishabh Vikram Singh <69898767+rishabh-glitch@users.noreply.github.com>
2022-09-28 10:42:08 +0530

    Update BookedParkingSlot.js

--- src/components/BookedParkingSlot.js
+++ src/components/BookedParkingSlot.js
@@ -2,6 +2,7 @@ import React,{useState,useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import HeaderComponent from './HeaderComponent'
 import ParkingSlotService from '../services/ParkingSlotService'
+import { ToastContainer,toast } from 'react-toastify';
 
 const BookedParkingSlot = () => {
     
@@ -10,7 +11,7 @@ const BookedParkingSlot = () => {
     useEffect(() => {
         getAllParkingSlots();
     }, [])
-
+    
     const getAllParkingSlots=()=>{
         ParkingSlotService.getAllParkingSlots().then((response)=>{
             setParkingSlots(response.data)
@@ -23,17 +24,22 @@ const BookedParkingSlot = () => {
     const deleteSlot = (parkingSlotId)=>{
         ParkingSlotService.deleteParkingSlot(parkingSlotId).then((response)=>{
             getAllParkingSlots();
+            deleteButton();
         }).catch(error=>{
             console.log(error);
         })
     }
 
+    const deleteButton=()=>{
+        toast.success("The record has been deleted");
+    };
   return (
     <div style={{backgroundColor:"#d4d3eb",height:"650px"}}>
+        <ToastContainer/>
         <HeaderComponent/>
         <br></br>
         <br></br>
-    
+        <Link to="/bookparkingslot" className='btn btn-primary' style={{float:"right",marginRight:"50px",textDecoration:"none"}}> + BOOK SLOT </Link>
         <h2 className='text-center' style={{fontFamily:"auto",textDecoration:"auto"}}>Booked Slots</h2>
         <table className = "table container table-bordered table-striped table-hover" style={{backgroundColor:"aliceblue",marginTop:"40px",borderRadius:"25px"}}>
             <thead style={{padding:"20px",textAlign:"center"}}>
@@ -69,7 +75,7 @@ const BookedParkingSlot = () => {
         </table>
         <div>
 
-        <Link to="/bookparkingslot" className='btn btn-primary' style={{float:"right",marginRight:"50px",textDecoration:"none"}}> + BOOK SLOT </Link>
+        
         </div>
         
     </div>
