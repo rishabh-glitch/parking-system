142be5a3217be82575cb5d820f5aea828469a91a
Rishabh Vikram Singh <69898767+rishabh-glitch@users.noreply.github.com>
2022-09-28 17:05:57 +0530

    Update HeaderComponent.js

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -16,21 +16,11 @@ const HeaderComponent = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto" >
             <Nav.Link href="#home" style = {{color:"aliceblue"}}>Home</Nav.Link>
-            <Link to="/bookparkingslot" className='nav-link active' style = {{color:"aliceblue"}}>BookSlots</Link>
+            {/* <Link to="/bookparkingslot" className='nav-link active' style = {{color:"aliceblue"}}>BookSlots</Link> */}
             <Nav.Link href="#link" style = {{color:"aliceblue"}}>Create Premise</Nav.Link>
             <Nav.Link href="#link" style = {{color:"aliceblue"}}>Create Floor</Nav.Link>
-            <Nav.Link href="/createvehicle" style = {{color:"aliceblue"}}>Create Vehicle</Nav.Link>
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
+            <Nav.Link href="/bookedparkingslot" style = {{color:"aliceblue"}}>Booked Slots</Nav.Link>
+            <Link className='btn btn-danger'to="/">Logout</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
