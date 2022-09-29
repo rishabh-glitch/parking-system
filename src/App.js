69243eaf8522503b09a2f4fcaebc270c132f8897
Rishabh Vikram Singh <69898767+rishabh-glitch@users.noreply.github.com>
2022-09-29 19:32:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import BookedParkingSlot from './components/BookedParkingSlot';
 import CheckAvailabilityComponent from './components/CheckAvailabilityComponent';
 import AddVehicleComponent from './components/AddVehicleComponent';
 // import Home from './components/Home';
+import SavedPremise from './components/SavedPremise';
 import Login from './components/Login';
 import UserHeader from './components/UserHeader';
 import SignUp from './components/SignUp';
+import CreatePremiseComponent from './components/CreatePremiseComponent'
+import ParkedVehicle from './components/ParkedVehicle'
 // import Signup from './components/Signup'
 // import About from './components/About'
 
@@ -25,6 +28,9 @@ function App() {
           {/* {/* <Route path ="/signup" component={Signup}></Route> */}
           <Route path ="/userhome" component={UserHeader}></Route>
           <Route path ="/login" component={Login}></Route>
+          <Route path ="/createpremisecomponent" component={CreatePremiseComponent}></Route>
+          <Route path = "/parkedvehicle" component = {ParkedVehicle}></Route>
+          <Route path ="/parkingpremise" component={SavedPremise}></Route>
           <Route path ="/home" component={HeaderComponent}></Route> 
           <Route path = "/bookparkingslot" component = {BookParkingSlotComponent}></Route>
           <Route path = "/bookedparkingslot" component = {BookedParkingSlot}></Route>
