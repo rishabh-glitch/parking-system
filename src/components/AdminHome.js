09c98a50323318e8b6e3815bb2426b4b940aa3d5
Rishabh Vikram Singh <69898767+rishabh-glitch@users.noreply.github.com>
2022-09-29 22:50:49 +0530

    Create AdminHome.js

---
+++ src/components/AdminHome.js
@@ -0,0 +1,33 @@
+
+import React from "react";
+import HeaderComponent from "./HeaderComponent";
+
+const AdminHome=()=>{
+
+        return (
+            //  <div className="container-fluid p-0 m-0">
+            <div>
+
+            <HeaderComponent/>
+            <div style={{background:"cadetblue",height:"650px",textAlign:"center",paddingTop:"200px",fontFamily:"monospace"}}>
+            <h1> Admin Module</h1>
+            </div>
+
+
+            
+            
+           
+            
+    
+    
+    
+            
+    
+             </div>
+    
+        );
+    
+    
+    };
+    
+    export default AdminHome;
