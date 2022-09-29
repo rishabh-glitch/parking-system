a8836b518c50b947fb0d43357a57cecefae97cdb
Rishabh Vikram Singh <69898767+rishabh-glitch@users.noreply.github.com>
2022-09-29 19:36:05 +0530

    Update VehicleService.js

--- src/services/VehicleService.js
+++ src/services/VehicleService.js
@@ -6,5 +6,11 @@ class VehicleService{
     saveVehicle(vehicle){
         return axios.post(VEHICLE_BASE_REST_API_URL + '/add',vehicle);
     }
+    getAllVehicle(){
+        return axios.get(VEHICLE_BASE_REST_API_URL+'/users');
+    }
+    deleteVehicle(vehicleId){
+        return axios.delete(VEHICLE_BASE_REST_API_URL+'/users/'+vehicleId);
+    }
 }
 export default new VehicleService();
