6772161403b7e4f30bf53bff77e0e2235e555f8a
Rishabh Vikram Singh <69898767+rishabh-glitch@users.noreply.github.com>
2022-09-29 22:52:02 +0530

    Create UserHome.js

---
+++ src/components/UserHome.js
@@ -0,0 +1,30 @@
+
+
+import UserHeader from "./UserHeader";
+
+import React from "react";
+
+const UserHome=()=>{
+
+        return (
+             <div className="container-fluid p-0 m-0">
+            <UserHeader/>
+            <h1> WELCOME TO ONLINE VEHICLE PARKING BOOKING SYSTEM</h1>
+
+            <h3>Are you fed up of searching a slot everytime? Now you can pre-book a slot with us!! </h3>
+            
+            <img src = "https://img.freepik.com/free-vector/underground-parking-flat-composition-with-doodle-character-driver-looking-free-slot-among-parked-cars_1284-62522.jpg?w=826&t=st=1664461920~exp=1664462520~hmac=0b1dbd42e5c3acfc73b0f8184bdf0a50ebbcf1173c0635c668c9759b0356d642" width="1400" height="430" alt = " image"/>
+            
+    
+    
+    
+            
+    
+             </div>
+    
+        );
+    
+    
+    };
+    
+    export default UserHome;
