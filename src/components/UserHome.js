bb00453d28db0f08412e3fd32a50532b2228eb4d
Rishabh Vikram Singh <69898767+rishabh-glitch@users.noreply.github.com>
2022-10-03 10:46:39 +0530

    Update UserHome.js

--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -13,7 +13,7 @@ const UserHome=()=>{
 
             <h3>Are you fed up of searching a slot everytime? Now you can pre-book a slot with us!! </h3>
             
-            <img src = "https://img.freepik.com/free-vector/underground-parking-flat-composition-with-doodle-character-driver-looking-free-slot-among-parked-cars_1284-62522.jpg?w=826&t=st=1664461920~exp=1664462520~hmac=0b1dbd42e5c3acfc73b0f8184bdf0a50ebbcf1173c0635c668c9759b0356d642" width="1400" height="430" alt = " image"/>
+            <img src = "https://img.freepik.com/free-vector/underground-parking-flat-composition-with-doodle-character-driver-looking-free-slot-among-parked-cars_1284-62522.jpg?w=826&t=st=1664461920~exp=1664462520~hmac=0b1dbd42e5c3acfc73b0f8184bdf0a50ebbcf1173c0635c668c9759b0356d642" width="1349px" height="470px" alt = " image"/>
             
     
     
